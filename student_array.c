d59b602f929a5f31f284e68b811918300f253caf
CyanBh <140052973+CyanBh@users.noreply.github.com>
2023-10-09 19:03:03 +0000

    better student array

--- student_array.c
+++ student_array.c
@@ -2,38 +2,42 @@
 */
 #include <stdio.h>
 
-void display_student_average();
+
 
 int main ()
 {
-    display_student_average();
-	return 0;
-
-}
 
 
-void  display_student_average()
-{
-    	int totalStudents, totalMarks=0; //number of students and the sum of their marks
+
+    int total_students, total_marks=0; //number of students and the sum of their marks
 
 	printf("Enter the number of students: ");
-	scanf ("%d", &totalStudents);
+	scanf ("%d", &total_students);
 
-	int studentMarks[totalStudents];
+	int arr_student[total_students];
 
-	for (int counter = 1; counter <= totalStudents; counter++	)
+	for (int i = 1; i <= total_students; i++	)
 	{
 		int marks;
 
-		printf("\n\nEnter the marks of the student number %d: ",counter);
+		printf("\n\nEnter the marks of the student number %d: ",i);
 		scanf("%d",&marks);//stores the values to be entered in the array
 
-		studentMarks[counter] = marks;
-		printf ("Marks recorded %d of %d", counter, totalStudents);
-		totalMarks = totalMarks + marks;
+		arr_student[i] = marks;
+		printf ("Marks recorded %d of %d", i, total_students);
+		total_marks = total_marks + marks;
 	}
 
-	float averageMarks = totalMarks/totalStudents;
+	float average = total_marks/total_students;
 
-	printf("\n\nThe average marks of %d students is %f\n ", totalStudents, averageMarks);
+	printf("\n\nThe average marks of %d students is %f ", total_students, average);
+
+
+
+    return 0;
+
+
 }
+
+
+
