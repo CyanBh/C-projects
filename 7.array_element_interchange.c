2f9a70592651f9186ef1e6106446dfce44063d13
CyanBh <140052973+CyanBh@users.noreply.github.com>
2023-10-10 04:23:28 +0000

    array_swap done

--- 7.array_element_interchange.c
+++ 7.array_element_interchange.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//incomplete
+void swap(int*, int*);
 
 int main()
 {
@@ -7,8 +7,8 @@ int main()
     printf("Enter the number of elements in the array: ");
     scanf("%d",&num);
 
-    int my_arr[num],temp1_arr[num], temp2_arr[num], new_arr[num];
-
+    int my_arr[num];
+    
 	for (int i=0; i<num; i++)//this part puts the values in the arrays;
 	{
 		int val;
@@ -16,37 +16,35 @@ int main()
 		printf("Enter the value of element %d  ",i);
 		scanf("%d",&val);
 
-        my_arr[i]=val;//array that the user sees
+        my_arr[i]=val;
 
-
-        if (i % 2 == 0)//for even number index, store in old array
-        {
-            temp1_arr[i] = val;
-            temp1_arr[i+1] = 0;
-        }
-		
-
-        if (i % 2 != 0)//for odd number index, store in temporary array
-        {
-            temp2_arr[i] = val;
-            temp2_arr[i-1] = 0;
-            temp2_arr[i+1] = 0;
-            
-        }
 	}
+        printf("original array\n");
 
-    for(int i = 0; i<num ; i++)
+        for(int i = 0; i<num ; i++) //display my_arr
     {
-        printf("%d    %d    %d\n",my_arr[i],temp1_arr[i],temp2_arr[i]);
+        printf("%d\n",my_arr[i]);
+    }
+
+    for (int i = 1; i<num-1; i= i+2)
+    {
+        swap( &my_arr[i], &my_arr[i+1]);
+    }
+        printf("swapped array\n");
+        for(int i = 0; i<num ; i++)//display my_arr after swapping
+    {
+        printf("%d\n",my_arr[i]);
     }
 
 
 
-
-
-
-
-
-
     return 0;
+}
+
+void swap(int* a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
