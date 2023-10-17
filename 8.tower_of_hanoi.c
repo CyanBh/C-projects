37f82eed94c8c60f41e03d9230fc72176b8e70be
CyanBh <140052973+CyanBh@users.noreply.github.com>
2023-10-17 11:08:17 +0000

    Tower of Hanoi has been done

---
+++ 8.tower_of_hanoi.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void tower(int n, char from_rod, char aux_rod, char to_rod);
+
+int main()
+{
+    int n;
+    printf("Enter the number of disks: ");
+    scanf ("%d",&n);
+
+    tower(n,'A','C','B');
+    return 0;
+}
+
+void tower(int n ,char beg, char end, char aux)
+{
+    if(n<=0)
+        printf("\nThe number of disks provided must be positive and greater than zero");
+
+    else if(n==1)
+        printf("\nMoved disk from %c to %c", beg, end);
+
+    else
+    {
+        tower(n-1, beg, aux, end);
+        printf("\nMoved disk from %c to %c", beg, end);
+        tower(n-1, aux, end, beg);
+    }
+
+}
