a7a91767efdecfeab82534d427ccae93820ead98
CyanBh <140052973+CyanBh@users.noreply.github.com>
2023-11-04 19:35:47 +0000

    incomplete bro lol

---
+++ assignment_pointers/4.ptr.c
@@ -0,0 +1,34 @@
+/*Write a C program that calculates the sum of elements in an integer array
+using pointer arithmetic.*/
+#include <stdio.h>
+int sum_array(int*);
+int main()
+{
+    int n,sum;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        printf("element %d: ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    sum = sum_array(arr);
+    printf("\nThe sum of elements of the array is %d",sum);
+
+    return 0;
+
+}
+
+int sum_array(int* p)
+{
+    int sum = 0;
+
+    while(p!=NULL)
+    {
+        sum += *p;
+        p++;
+    }
+
+    return sum;
+}
